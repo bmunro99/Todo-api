0d1b1ca41dfbe23d02a968fdba224211208c833c
Scott Munro <bmunro@us.ibm.com>
2016-07-11 10:12:35 -0500

    Add POST for todos

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
 
+app.use(bodyParser.json());
 // static test data
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Feed the cat',
-	completed: true
-}];
+// var todos = [{
+// 	id: 1,
+// 	description: 'Meet ben for lunch',
+// 	completed: false
+// }, {
+// 	id: 2,
+// 	description: 'Go to market',
+// 	completed: false
+// }, {
+// 	id: 3,
+// 	description: 'Feed the cat',
+// 	completed: true
+// }];
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -43,6 +47,18 @@ app.get('/todos/:id', function (req, res){
 		res.status(404).send();
 	}
 });
+
+// POST /todos
+app.post('/todos', function(req, res){
+	var body = req.body;
+	body.id = todoNextId;
+	todoNextId++;
+
+	todos.push(body);
+	//console.log('description: ' + body.description);
+	res.json(body);
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT);
