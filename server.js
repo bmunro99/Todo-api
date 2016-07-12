39dccff50d15f71c7390b2ab8da8a8a5bb330be5
Scott Munro <bmunro@us.ibm.com>
2016-07-12 13:19:12 -0500

    add postgres for production

--- server.js
+++ server.js
@@ -35,27 +35,8 @@ app.get('/todos', function(req, res) {
 			res.json(todos);
 		}, function(e) {
 			res.status(500).send();
-		})
-		// var filteredTodos = todos;
-		// console.log(filteredTodos);
-
-	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-	// 	filteredTodos = _.where(filteredTodos, {
-	// 		completed: true
-	// 	});
-	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-	// 	filteredTodos = _.where(filteredTodos, {
-	// 		completed: false
-	// 	});
-	// }
-
-	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
-	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-	// 	});
-	// }
-
-	// res.json(filteredTodos);
+		});
+	
 });
 
 // GET /todos/:id
