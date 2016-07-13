89c5b2a33f5ff0158e10aecd1658ba56d2517ead
Scott Munro <bmunro@us.ibm.com>
2016-07-13 14:05:41 -0500

    Convert email to lower case

---
+++ models/user.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('usser', {
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [7, 100]
+			}
+		}
+	}, {
+		hooks: {
+			beforeValidate: function (user, options) {
+				// user.email
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
+			}
+		}
+	});
+};
